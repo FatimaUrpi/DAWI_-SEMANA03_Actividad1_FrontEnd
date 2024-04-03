721971f21ae07162778672d1a628fd5b6873adb5
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-04-02 23:18:55 -0500

    creacion formulario

---
+++ src/app/components/add-modalidad/add-modalidad.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AppMaterialModule } from '../../app.material.module';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@Component({
+  selector: 'app-add-modalidad',
+  standalone: true,
+  imports: [AppMaterialModule],
+  templateUrl: './add-modalidad.component.html',
+  styleUrl: './add-modalidad.component.css'
+})
+export class AddModalidadComponent {
+
+    
+
+}
