0b1e6f70e3d588d060dad886227ac34de4d1b282
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-04-03 23:12:04 -0500

    ajustes

--- src/app/components/add-modalidad/add-modalidad.component.ts
+++ src/app/components/add-modalidad/add-modalidad.component.ts
@@ -2,16 +2,48 @@ import { Component } from '@angular/core';
 import { AppMaterialModule } from '../../app.material.module';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DeporteService } from '../../services/deporte.service';
+import { Deporte } from '../../models/deporte.model';
+import { FormsModule } from '@angular/forms';
+import { ModalidadService } from '../../services/modalidad.service';
+import { Modalidad } from '../../models/modalidad.model';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-add-modalidad',
   standalone: true,
-  imports: [AppMaterialModule],
+  imports: [AppMaterialModule, FormsModule, CommonModule],
   templateUrl: './add-modalidad.component.html',
   styleUrl: './add-modalidad.component.css'
 })
 export class AddModalidadComponent {
 
-    
+      lstDeportes : Deporte[] = [];
+
+      objModalidad : Modalidad  ={
+          nombre : "",
+          numHombres : 0,
+          numMujeres : 0,
+          edadMaxima : 0,
+          edadMinima : 0,
+          sede : "",
+          deporte : {
+              idDeporte : -1
+          }
+      }
+
+      constructor(private deporteService : DeporteService, 
+                  private modalidadService:ModalidadService){
+
+              this.deporteService.listaDeporte().subscribe(
+                    x => this.lstDeportes = x
+              );
+      }
+
+      inserta(){
+              this.modalidadService.registraModalidad(this.objModalidad).subscribe(
+                  x =>  Swal.fire({icon: 'info',title: 'Resultado del Registro - Jacinto',text: x}) 
+              );
+      }
 
 }
